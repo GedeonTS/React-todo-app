18facfb5d386733a49f3d4a928fde3bba5f13bcc
GedeonTS <gedeontshobohwa1@gmail.com>
2022-05-31 12:08:23 +0200

    create componnents

---
+++ src/components/TodoContainer.js
@@ -0,0 +1,32 @@
+import React from "react"
+import TodosList from "./TodosList";
+
+class TodoContainer extends React.Component {
+    state = {
+        todos: [
+            {
+                id: 1,
+                title: "Setup development environment",
+                completed: true
+            },
+            {
+                id: 2,
+                title: "Develop website and add content",
+                completed: false
+            },
+            {
+                id: 3,
+                title: "Deploy to live server",
+                completed: false
+            }
+        ]
+    };
+    render() {
+        return (
+            <ul>
+                <TodosList todos={this.state.todos} />
+            </ul>
+        )
+    }
+}
+export default TodoContainer
---
+++ src/components/TodosList.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+class TodosList extends React.Component {
+    render() {
+        return (
+            <div>TodosList</div>
+        )
+    }
+}
+
+export default TodosList
